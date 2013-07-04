de7d32abc6ea04b2dce958f182dbbbd7f0adc07a
Fredrik Ronquist <Fredrik.Ronquist@nrm.se>
2013-07-04 13:01:53 +0000

    Comment change

--- src/mb.c
+++ src/mb.c
@@ -1,2 +1,2 @@
 char version[]="$Id: mb.c,v 1.3 2011/07/07 Exp $";
-const char* const svnRevisionMbC="$Rev$";   /* Revision keyword which is expanded/updated by svn on each commit/update*/
+const char* const svnRevisionMbC="$Rev$";   /* Revision keyword which is expanded/updated by svn on each commit/update */
