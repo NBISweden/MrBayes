2132d53811b3e477ea5518f25c370229c10f53d7
Andreas Kusalananda Kähäri <andreas.kahari@bils.se>
2016-07-16 14:23:09 +0200

    Put back definitions of macros M_PI and M_PI_2. Surprisingly, these are not part of the C standard <math.h>.

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,10 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+/* M_PI and M_PI_2 not part of standard C */
+#define M_PI    3.14159265358979323846
+#define M_PI_2  1.57079632679489661923
+
 struct MrBComplex
 {
     MrBFlt re;
