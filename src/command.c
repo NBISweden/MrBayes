NBISweden/MrBayes

commit ea2712ec4c8f60efacf9c1cc00f11c2f40d3e577

fixed bug in the prior distribution in ILN clock model, see #250

src/command.c
@@ -1561,7 +1561,7 @@ int DoCalibrateParm (char *parmName, char *tkn)
             {
             calibrationPtr->LnPriorProb     = &LnPriorProbOffsetLognormal_Param_Offset_Mean_Sd;
             calibrationPtr->LnPriorRatio    = &LnProbRatioOffsetLognormal_Param_Offset_Mean_Sd;
-            calibrationPtr->min             = BRLENS_MIN + priorParams[0];
+            calibrationPtr->min             = priorParams[0];
             calibrationPtr->max             = POS_INFINITY;
             }
         else if (calPrior == standardGamma)
@@ -1575,7 +1575,7 @@ int DoCalibrateParm (char *parmName, char *tkn)
             {
             calibrationPtr->LnPriorProb     = &LnPriorProbOffsetGamma_Param_Offset_Mean_Sd;
             calibrationPtr->LnPriorRatio    = &LnProbRatioOffsetGamma_Param_Offset_Mean_Sd;
-            calibrationPtr->min             = BRLENS_MIN + priorParams[0];
+            calibrationPtr->min             = priorParams[0];
             calibrationPtr->max             = POS_INFINITY;
             }
 

src/bayes.c
@@ -1059,9 +1059,9 @@ int ReinitializeMrBayes (void)
     chainParams.orderTaxa = NO;                      /* should taxa be ordered in output trees?       */
     chainParams.append = NO;                         /* append to previous analysis?                  */
     chainParams.autotune = YES;                      /* autotune?                                     */
-    chainParams.tuneFreq = 100;                      /* autotuning frequency                          */
+    chainParams.tuneFreq = 1000;                     /* autotuning frequency                          */
     chainParams.checkPoint = YES;                    /* should we checkpoint the run?                 */
-    chainParams.checkFreq = 2000;                    /* check-pointing frequency                      */
+    chainParams.checkFreq = 1000;                    /* check-pointing frequency                      */
     chainParams.diagnStat = AVGSTDDEV;               /* mcmc diagnostic to use                        */
 
     /* sumt parameters */

src/bayes.h
@@ -365,8 +365,6 @@ typedef float CLFlt;        /* single-precision float used for cond likes (CLFlt
 #define POS_MAX                 1E25f
 #define POS_INFINITY            1E25f
 #define NEG_INFINITY            -1000000.0f
-#define POSREAL_MIN             1E-25f
-#define POSREAL_MAX             1E25f
 
 /* SK:
  * Used when doing reversible jump over stationary and directional

src/best.c
@@ -1600,15 +1600,15 @@ int Move_NodeSliderGeneTree (Param *param, int chain, RandLong *seed, MrBFlt *ln
             /* prior ratio */
             if (p->left != NULL)
                 {
-                (*lnPriorRatio) -= LnProbLogNormal_LinMean_LogVar (rate[p->index], nu*oldLeftLength, rate[p->left->index]);
-                (*lnPriorRatio) -= LnProbLogNormal_LinMean_LogVar (rate[p->index], nu*oldRightLength, rate[p->right->index]);
-                (*lnPriorRatio) += LnProbLogNormal_LinMean_LogVar (rate[p->index], nu*p->left->length, rate[p->left->index]);
-                (*lnPriorRatio) += LnProbLogNormal_LinMean_LogVar (rate[p->index], nu*p->right->length, rate[p->right->index]);
+                (*lnPriorRatio) -= LnProbLogNormal_Mean_LogVar (rate[p->index], nu*oldLeftLength, rate[p->left->index]);
+                (*lnPriorRatio) -= LnProbLogNormal_Mean_LogVar (rate[p->index], nu*oldRightLength, rate[p->right->index]);
+                (*lnPriorRatio) += LnProbLogNormal_Mean_LogVar (rate[p->index], nu*p->left->length, rate[p->left->index]);
+                (*lnPriorRatio) += LnProbLogNormal_Mean_LogVar (rate[p->index], nu*p->right->length, rate[p->right->index]);
                 }
             if (p->anc->anc != NULL)
                 {
-                (*lnPriorRatio) -= LnProbLogNormal_LinMean_LogVar (rate[p->anc->index], nu*oldPLength, rate[p->index]);
-                (*lnPriorRatio) += LnProbLogNormal_LinMean_LogVar (rate[p->anc->index], nu*p->length, rate[p->index]);
+                (*lnPriorRatio) -= LnProbLogNormal_Mean_LogVar (rate[p->anc->index], nu*oldPLength, rate[p->index]);
+                (*lnPriorRatio) += LnProbLogNormal_Mean_LogVar (rate[p->anc->index], nu*p->length, rate[p->index]);
                 }
 
             /* update effective evolutionary lengths */

src/mcmc.c
@@ -8292,8 +8292,8 @@ MrBFlt LogPrior (int chain)
                 {
                 branch = t->allDownPass[i];
                 if (branch->length > 0.0)  // not ancestral fossil
-                    lnPrior += LnProbLogNormal_LinMean_LogVar (st[branch->anc->index], nu*branch->length, st[branch->index]);
-                }
+                    lnPrior += LnProbLogNormal_Mean_LogVar (st[branch->anc->index], nu*branch->length, st[branch->index]);
+                }   // mean is on natural scale and variance is on log scale
             }
         else if (p->paramType == P_WNVAR)
             {
@@ -8346,8 +8346,8 @@ MrBFlt LogPrior (int chain)
                 {
                 branch = t->allDownPass[i];
                 if (branch->length > 0.0)  // not ancestral fossil
-                    lnPrior += LnProbLogNormal_LinMean_LogVar (1.0, nu, st[branch->index]);
-                }
+                    lnPrior += LnProbLogNormal_Mean_Var (1.0, nu, st[branch->index]);
+                }   // both mean and variance are on natural scale
             }
         else if (p->paramType == P_IGRVAR)
             {
@@ -9074,7 +9074,7 @@ int LnFossilizedBDPriorRandom (Tree *t, MrBFlt clockRate, MrBFlt *prob, MrBFlt *
         lambda[i] = netDiver[i] / (1.0 - turnOver[i]);
         mu[i] = lambda[i] * turnOver[i];
         psi[i] = mu[i] * sampProp[i] / (1.0 - sampProp[i]);
-        rho[i] = 1E-10;  // to avoid prob being -inf
+        rho[i] = 1E-20;  // to avoid prob being -inf
         }
     rho[sl] = sF;  // 0 < sF <= 1
 
@@ -9278,7 +9278,7 @@ int LnFossilizedBDPriorDiversity (Tree *t, MrBFlt clockRate, MrBFlt *prob, MrBFl
         lambda[i] = netDiver[i] / (1.0 - turnOver[i]);
         mu[i] = lambda[i] * turnOver[i];
         psi[i] = mu[i] * sampProp[i] / (1.0 - sampProp[i]);
-        rho[i] = 1E-10;  // to avoid prob being -inf
+        rho[i] = 1E-20;  // to avoid prob being -inf
         }
     lambda[sl] = lambda[sl-1];  netDiver[sl] = netDiver[sl-1];
     mu[sl]  = mu[sl-1];         turnOver[sl] = turnOver[sl-1];
@@ -17214,7 +17214,7 @@ int RunChain (RandLong *seed)
                     stopChain = YES;
                     for (i=0; i<numTopologies; i++)
                         {
-                        f=-1.0;
+                        f = -1.0;
                         if (chainParams.stat[i].numPartitions == 0)
                             {
                             if (strcmp(modelParams[0].topologyPr,"Speciestree") == 0)

src/model.c
@@ -8879,9 +8879,9 @@ int DoPrsetParm (char *parmName, char *tkn)
                         if (!strcmp(modelParams[i].cppMultDevPr,"Fixed"))
                             {
                             sscanf (tkn, "%lf", &tempD);
-                            if (tempD < POSREAL_MIN || tempD > POSREAL_MAX)
+                            if (tempD < POS_MIN || tempD > POS_MAX)
                                 {
-                                MrBayesPrint ("%s   The log standard deviation of rate multipliers must be in the range %f - %f\n", spacer, POSREAL_MIN, POSREAL_MAX);
+                                MrBayesPrint ("%s   The log standard deviation of rate multipliers must be in the range %f - %f\n", spacer, POS_MIN, POS_MAX);
                                 return (ERROR);
                                 }
                             modelParams[i].cppMultDevFix = tempD;
@@ -23363,15 +23363,15 @@ void SetUpMoveTypes (void)
     mt->name = "rjMCMC among Relaxed Clock Models";
     mt->shortName = "rjMCMC_RCL";
     mt->tuningName[0] = "Ratio between variances";
-    mt->shortTuningName[0] = "delta";
+    mt->shortTuningName[0] = "delta";  // w = sigma_L/sigma_G
     mt->applicableTo[0] = MIXEDBRCHRATES;
     mt->nApplicable = 1;
     mt->moveFxn = &Move_RelaxedClockModel;
     mt->relProposalProb = 25.0;
     mt->numTuningParams = 1;
-    mt->tuningParam[0] = 1.0;
-    mt->minimum[0] = 0.0001;
-    mt->maximum[0] = 1000.0;
+    mt->tuningParam[0] = 1.5;
+    mt->minimum[0] = 0.01;
+    mt->maximum[0] = 100.0;
     mt->parsimonyBased = NO;
     mt->level = STANDARD_USER;
     

src/utils.c
@@ -13129,27 +13129,28 @@ MrBFlt LnRatioLogNormal (MrBFlt mu, MrBFlt sigma, MrBFlt xNew, MrBFlt xOld)
 
 
 /* Log probability for a value drawn from a lognormal distribution;
-   parameters are mean and variance of value (not of log value) */
-MrBFlt LnProbLogNormal_LinMean_LogVar (MrBFlt mean, MrBFlt var, MrBFlt x)
+   parameters are natural mean (not of log value) and variance on log scale */
+MrBFlt LnProbLogNormal_Mean_LogVar (MrBFlt mean, MrBFlt sigma2, MrBFlt x)
 {
     MrBFlt  mu, sigma;
     
-    sigma = sqrt(var);
-    mu    = log(mean) - sigma * sigma / 2.0;
-    
+    mu    = log(mean) - sigma2 / 2.0;
+    sigma = sqrt(sigma2);
+
     return LnProbLogNormal(mu, sigma, x);
 }
 
 
-/* Log ratio for two values drawn from a lognormal distribution */
-MrBFlt LnRatioLogNormal_LinMean_LogVar (MrBFlt mean, MrBFlt var, MrBFlt xNew, MrBFlt xOld)
+/* Log probability for a value drawn from a lognormal distribution;
+   parameters are mean and variance of value (not of log value) */
+MrBFlt LnProbLogNormal_Mean_Var (MrBFlt mean, MrBFlt var, MrBFlt x)
 {
     MrBFlt  mu, sigma;
-
-    sigma = sqrt(var);
+    
+    sigma = sqrt(log(1.0 + var / (mean*mean)));
     mu    = log(mean) - sigma * sigma / 2.0;
 
-    return LnRatioLogNormal (mu, sigma, xNew, xOld);
+    return LnProbLogNormal(mu, sigma, x);
 }
 
 

commit 78f0c034665ea27c7647c10231f8e455bf980e62

fixed a bug introduced in commit ea2712e by reverting the changes to calibrationPtr->min

src/command.c
@@ -1561,7 +1561,7 @@ int DoCalibrateParm (char *parmName, char *tkn)
             {
             calibrationPtr->LnPriorProb     = &LnPriorProbOffsetLognormal_Param_Offset_Mean_Sd;
             calibrationPtr->LnPriorRatio    = &LnProbRatioOffsetLognormal_Param_Offset_Mean_Sd;
-            calibrationPtr->min             = priorParams[0];
+            calibrationPtr->min             = BRLENS_MIN + priorParams[0];
             calibrationPtr->max             = POS_INFINITY;
             }
         else if (calPrior == standardGamma)
@@ -1575,7 +1575,7 @@ int DoCalibrateParm (char *parmName, char *tkn)
             {
             calibrationPtr->LnPriorProb     = &LnPriorProbOffsetGamma_Param_Offset_Mean_Sd;
             calibrationPtr->LnPriorRatio    = &LnProbRatioOffsetGamma_Param_Offset_Mean_Sd;
-            calibrationPtr->min             = priorParams[0];
+            calibrationPtr->min             = BRLENS_MIN + priorParams[0];
             calibrationPtr->max             = POS_INFINITY;
             }
 