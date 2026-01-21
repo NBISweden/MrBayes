9629b2b7d9f0334d5685d43850564a3a33c21049
gknightly <george.knightly@gmail.com>
2026-01-20 18:55:38 -0800

    fix: Cannot compile MrBayes on ARM64 with BEAGLE (#302)
    numFloatsPerVec only exists when SSE_ENABLED. Fix ARM64 BEAGLE builds
    where SSE is unavailable.

--- src/mcmc.c
+++ src/mcmc.c
@@ -5879,7 +5879,9 @@ int InitChainCondLikes (void)
         if (m->useBeagle == YES)
             {
             m->useVec = VEC_NONE;
+#           if defined (SSE_ENABLED)
             m->numFloatsPerVec = 0;
+#           endif
             }
 #       endif
 
